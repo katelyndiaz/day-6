8523cad558a8aafc1b950bc7351baef9ae4faf62
Katelyn D <katelynd@Apples-MacBook-Pro-10.local>
2019-08-12 14:58:15 -0400

    table practice

---
+++ ProjectTablePractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectTablePractice
+//
+//  Created by Katelyn D on 8/12/19.
+//  Copyright © 2019 Katelyn D. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
